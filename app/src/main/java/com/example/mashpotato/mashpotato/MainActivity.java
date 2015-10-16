86aa0a831901c3d0f95c8842945f0080f9ce047c
supantha <supantha.samanta@gmail.com>
2015-10-16 12:40:29 +0530

    Video recording button added

--- app/src/main/java/com/example/mashpotato/mashpotato/MainActivity.java
+++ app/src/main/java/com/example/mashpotato/mashpotato/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.mashpotato.mashpotato;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int CAPTURE_VIDEO_ACTIVITY=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button videobutton= (Button) findViewById(R.id.videobutton);
+        videobutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("android.media.action.VIDEO_CAPTURE");
+                startActivityForResult(intent,CAPTURE_VIDEO_ACTIVITY);
+            }
+        });
     }
 
     @Override
@@ -34,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
